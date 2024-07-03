f6e5d6fad6d217ff896042c20c1774af206bf2c5
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-07-03 15:14:15 +0200

    add frontend for admin to write on nfv

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -4,15 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState("")
-    const createToken = (name)=>{
+  const [user_uid, setUserUid] = useState("")
+  const [token, setToken]  = useState(null)
+    const createToken = (user_uid)=>{
     const URL = "http://localhost:7000"
     fetch(URL + '/createToken', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: name }),
+      body: JSON.stringify({ user_uid: user_uid }),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -23,11 +24,29 @@ function App() {
         console.error('Error:', error);
       });
   }
+  const submitForm = ()=>{
+    createToken(user_uid)
+    console.log(token)
+    writeToken().then(()=>console.log('token written'))
+  }
+
+  const writeToken = async()=>{
+     const ndef = new NDEFReader();
+    try {
+      await ndef.write({
+        records: [{ recordType: "url", data: "http://localhost:5173/"+user_uid }]
+      });
+      console.log("Write completed.");
+    } catch {
+      console.log("Write failed :-( try again.");
+    };
+  }
   return (
     <>
     <form>
-      <label htmlFor="">Insert name of the user</label>
-      <input type="text" id="name" name='name' value={name} onChange={()=>setName(name)}/>
+      <label htmlFor="">Insert uid of user</label>
+      <input type="text" id="name" name='name' value={user_uid} onChange={(e)=>setUserUid(e.target.value)}/>
+      <button type='button' onClick={submitForm}>Create token</button>
     </form>
     </>
   )
