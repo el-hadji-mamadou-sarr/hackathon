6ca9f53cacf71411a883aa0bee5803ad25529269
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-07-04 09:37:21 +0200

    add youssouf design in mobile app

--- elock-mobile-app/src/components/Connected.jsx
+++ elock-mobile-app/src/components/Connected.jsx
@@ -1,5 +1,25 @@
+import logo from "./Logo.png";
+import "./App.css";
+
 export const Connected = () => {
-    return (
-        <div>Connected 👍</div>
-    );
-}
+  return (
+    <div className="App">
+      <div className="top-bar">
+        <span className="top-bar-text">eLock</span>
+      </div>
+      <header className="App-header">
+        <div className="content">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="auth-message">
+            <h1 className="Home-message">
+              Bienvenue dans votre espace utilisateur
+            </h1>
+          </div>
+          <div className="Success-message1">
+            Authentification éffectué avec succès !
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
--- elock-mobile-app/src/components/Failed.jsx
+++ elock-mobile-app/src/components/Failed.jsx
@@ -1,5 +1,17 @@
+import logo from "./Logo.png";
+import "./App.css";
 export const Failed = () => {
-    return (
-        <div>Failed 👎</div>
-    );
-}
+  return (
+    <div className="App">
+      <div className="top-bar">
+        <span className="top-bar-text">eLock</span>
+      </div>
+      <header className="App-header">
+        <div className="content">
+          <img src={logo} className="App-logo_failed" alt="logo" />
+          <div className="Failed-message">Authentification échouée !</div>
+        </div>
+      </header>
+    </div>
+  );
+};
